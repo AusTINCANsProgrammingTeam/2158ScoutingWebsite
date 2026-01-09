27ee86e74a9807edf2d7725d2986bd06de42fbea
kennyL07 <kennysonle5.0@gmail.com>
2026-01-08 18:33:44 -0600

    use cors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const googleSpreadSheet =  require('google-spreadsheet');
 const googleAuth = require('google-auth-library');
 var sheet;
@@ -27,6 +28,7 @@ const port = 3000
 
 app.use(express.static('src'))
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.sendFile('src/index.html', {root: __dirname })
